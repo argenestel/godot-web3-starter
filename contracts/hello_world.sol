7eff0364fe2cbf4348d84a4f528f8c2d2d5103f9
argenestel <49126445+argenestel@users.noreply.github.com>
2024-08-30 07:19:47 +0530

    first contract

---
+++ contracts/hello_world.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+  uint256 public storedInteger;
+
+  function increment() public {
+    storedInteger += 1; 
+  }
+}
+
